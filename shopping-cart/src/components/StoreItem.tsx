c675a695f3099b477cf6ba722be3f565456a7b40
clearsky1359 <ali_hsd@yahoo.com>
2023-01-27 03:07:15 +0000

    stage 2

--- shopping-cart/src/components/StoreItem.tsx
+++ shopping-cart/src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import { formatCurrency } from "../utils/formatCurrency"
 type StoreItemProps={
     id:number,
@@ -9,7 +10,8 @@ type StoreItemProps={
 
 
 export function StoreItem({id, name, price, imgUrl }:StoreItemProps){
-    const quantity=1;
+    const{getItemQuantity, increaseCartQuantity,decreaseCartQuantity, removeFromCart}=useShoppingCart()
+    const quantity=getItemQuantity(id);
     return<Card className='h-100'>
             <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit:'cover'}}></Card.Img>
             <Card.Body className=" d-flex flex-column  ">
@@ -18,16 +20,16 @@ export function StoreItem({id, name, price, imgUrl }:StoreItemProps){
                     <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
                 <div className="mt-auto text-center">
-                    {quantity===0 ? (<Button className="w-100">+ ADD to Cart</Button>) : <div className='d-flex flex-column align-items-center' style={{gap:'0.5rem'}}>
+                    {quantity===0 ? (<Button className="w-100" onClick={()=>increaseCartQuantity(id)}>+ ADD to Cart</Button>) : <div className='d-flex flex-column align-items-center' style={{gap:'0.5rem'}}>
                         <div className="d-flex align-items-center justify-content-center" style={{gap:'0.5rem'}}>
-                            <Button>-</Button>
+                            <Button onClick={()=>decreaseCartQuantity(id)}>-</Button>
                             <div>
                                 <span className="fs-3">{quantity}</span> in Cart
                             </div>
-                            <Button>+</Button>
+                            <Button onClick={()=>increaseCartQuantity(id)}>+</Button>
                             </div>
                         </div>}
-                        <Button variant='danger' size='sm'>Remove</Button>
+                        <Button variant='danger' size='sm'onClick={()=>removeFromCart(id)}>Remove</Button>
                 </div>   
             </Card.Body>
         </Card>
