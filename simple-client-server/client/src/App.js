55cd0e9cfb79d49a2b93c0d0d21452cae5a3211a
clearsky1359 <ali_hsd@yahoo.com>
2023-03-02 21:31:18 +0000

    updated 5

--- simple-client-server/client/src/App.js
+++ simple-client-server/client/src/App.js
@@ -3,16 +3,49 @@ import './App.css';
 
 function App() {
 
-  const [data, setData]=useState('initial value')
+  const [data, setData]=useState([1,2])
+  const [test, setTest]=useState('')
 
   useEffect(()=>{
-    fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us88.gitpod.io/test').then(res=>res.json()).then(data=> console.log(data))
+    fetch('https://8081-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/test')
+    .then(res=>res.json()).
+    then(data=> {setData(data)
+    
+    })
 
   },[])
+  console.log(test)
+
+  const submit = e => {
+    e.preventDefault();
+
+    fetch('https://8081-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ test })
+    }).then(() => {
+      console.log('added');
+      setTest(''); // clear the input field after submitting
+      // fetch the data again to update the component with the new test
+      fetch('https://8081-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/test')
+        .then(res => res.json())
+        .then(data => {
+          setData(data);
+        });
+    });
+  };
 
   return (
     <>
-      <h1>{data}</h1>
+    {data.map((d,index) => <p key={index}>{d.test}</p>)}
+    <form onSubmit={submit}>
+      <label htmlFor='test'> Enter new test</label>
+      <input id='test' value={test} onChange={(e)=>{setTest(e.target.value)}}/>
+      <button> Submit </button>
+    </form>
+
     </>
   );
 }
--- simple-client-server/server/app.js
+++ simple-client-server/server/app.js
@@ -1,26 +1,78 @@
 //import modules 
-const express= require('express')
+const express=require('express')
+const mongoose=require('mongoose')
+
+
 const cors=require('cors')
 require('dotenv').config()
 
 //app
 const app=express();
 
+//DB
+mongoose.connect('mongodb+srv://nikan:Nikan2017@cluster0.i9tio.mongodb.net/?retryWrites=true&w=majority').then(()=>{console.log('connected to Database')})
+
+
 //middleware
 
-const corsOptions = {
-    origin: 'https://3000-clearsky135-reactprojec-pn9cq9isfm9.ws-us88.gitpod.io/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://3000-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io");
+  console.log('Cors test')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-  app.get('/test', cors(corsOptions), function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for only example.com.'})
-  })
+ 
+
+//Model
+
+const testSchema = new mongoose.Schema({
+  test: {
+    type: String,
+    required: true
+  }
+})
+
+const TestModel = mongoose.model('Test', testSchema);
+
 
 
 //routes 
-app.get('/test', (req,res)=>{res.status(200).json({message:'hooora'})})
+app.get('/test', async (req,res)=>{
+  
+
+  const tests=await TestModel.find()
+
+  res.status(200).json(tests)
+
+
+})
+
+
+app.post('/test', async (req, res) => {
+  try {
+    const { test } = req.body;
+
+    // Create a new task object
+    const task = new TestModel({
+      test
+    });
+
+    // Save the task to the database
+    await task.save();
+
+    // Send a response indicating success
+    res.status(201).json({ message: 'Task created successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+
 
 //server
-const PORT=process.env.PORT || 8080
+const PORT=process.env.PORT || 8081
 const server=app.listen(PORT,()=>console.log('connected........'))
