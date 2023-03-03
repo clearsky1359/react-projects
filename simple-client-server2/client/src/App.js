555010e8851485880f6656c48775f73d88fbdb67
clearsky1359 <ali_hsd@yahoo.com>
2023-03-03 03:16:08 +0000

    update 3

--- simple-client-server2/client/src/App.js
+++ simple-client-server2/client/src/App.js
@@ -5,14 +5,47 @@ import{useState, useEffect} from 'react';
 function App() {
 
   const [data, setData]=useState('');
+  const [name, setName]=useState('');
+  const [email, setEmail]=useState('');
+
   useEffect(()=>{
     fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/getAllTests')
     .then((res)=>res.json())
     .then(d=>setData(d))
 
   },[])
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/createTest', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name,
+    email
+  
+  })
+})
+.then(res => res.json())
+.then(out => console.log(out))
+.catch(error => console.error(error));
+
+
+}
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name</label>
+        <input id='name' value={name} onChange={e=>(setName(e.target.value))}/>
+        <br/>
+        <label htmlFor='email'>Email</label>
+        <input id='email' value={email} onChange={e=>(setEmail(e.target.value))}/>
+        <button>Submit</button>
+      </form>
+
       {data && data.map((r,index) => (
         <div key={index}>
           <p>{r.name}</p>
