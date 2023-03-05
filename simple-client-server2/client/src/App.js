e6b6a363e2ce3ae1d7dc563a42d7de4765a72412
clearsky1359 <ali_hsd@yahoo.com>
2023-03-05 22:36:46 +0000

    updated

--- simple-client-server2/client/src/App.js
+++ simple-client-server2/client/src/App.js
@@ -1,19 +1,16 @@
-import './App.css';
-
-import{useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-
-  const [data, setData]=useState('');
-  const [name, setName]=useState('');
-  const [email, setEmail]=useState('');
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-/////////////////////////////////////////////////////
-const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
+  const [images, setImages] = useState([]);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
-  }
+  };
 
   const handleSubmit2 = async (event) => {
     event.preventDefault();
@@ -34,75 +31,98 @@ const [file, setFile] = useState(null);
     }
   };
 
-  
-
-////////////////////////////////////////////////
-
-
-
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/getAllTests')
-    .then((res)=>res.json())
-    .then(d=>setData(d))
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  }, []);
 
-  },[])
+  useEffect(() => {
+    fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/images')
+      .then((res) => res.json())
+      .then((images) => setImages(images));
+  }, []);
 
-const handleSubmit=(e)=>{
-  e.preventDefault()
-  fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/getAllTests', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    name,
-    email
-  
-  })
-})
-.then(res => res.json())
-    .then(newData => {
-      setData(prevData => [...prevData, newData]);
-      setName('');
-      setEmail('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/getAllTests', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
     })
-    .catch(error => console.error(error));
-
-}
-
-
+      .then((res) => res.json())
+      .then((newData) => {
+        setData((prevData) => [...prevData, newData]);
+        setName('');
+        setEmail('');
+      });
+  };
 
   return (
-    <>
+    <div className="App">
       <form onSubmit={handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input id='name' value={name} onChange={e=>(setName(e.target.value))}/>
-        <br/>
-        <label htmlFor='email'>Email</label>
-        <input id='email' value={email} onChange={e=>(setEmail(e.target.value))}/>
-        <button>Submit</button>
-        <hr/><hr/>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Add</button>
       </form>
+
+      <h2>Data:</h2>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data.map((item) => (
+            <li key={item._id}>
+              {item.name} - {item.email}
+            </li>
+          ))}
+        </ul>
+      )}
+
       <form onSubmit={handleSubmit2}>
         <input type="file" onChange={handleFileChange} />
-        {isLoading && <p>Loading...</p>}
-        <button type="submit">Submit</button>
+        <button type="submit">Upload</button>
       </form>
 
 
 
+      <button onClick={() => {
+        fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/images')
+          .then((res) => res.json())
+          .then((images) => {
+            setImages(images)
+          
+            console.log(images)
+          });
+      }}>Show Images</button>
 
-      {data && data.map((r,index) => (
-        <div key={index}>
-          <p>{r.name}</p>
-          <p>{r.email}</p>
-          <hr/>
-        </div>
+      <h2>Images:</h2>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={`data:${image.contentType};base64,${image.data}`}
+          alt={image.name}
+          width="200"
+          height="200"
+        />
       ))}
-    </>
+    </div>
   );
 }
-
 
 export default App;
--- simple-client-server2/server/app.js
+++ simple-client-server2/server/app.js
@@ -1,66 +1,67 @@
-//import modules
-const express=require('express')
-const mongoose=require('mongoose')
-const cors=require('cors')
-const routerTest=require('./routes/test2')
-const multer  = require('multer');
+// import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routerTest = require('./routes/test2');
+const multer = require('multer');
 require('dotenv').config();
 
-//app
-const app=express()
+// app
+const app = express();
 
-//middleware
-app.use(express.json())
-//app.use(cors({origin:'https://3000-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io'}))
-// Configure CORS headers
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// middleware
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.URI).then(()=>console.log('Connected to DB'))
+mongoose.connect(process.env.URI).then(() => console.log('Connected to DB'));
 
-//////////////////////////////////////////////////
 // Create a schema for the image
 const imageSchema = new mongoose.Schema({
-    name: String,
-    data: Buffer,
-    contentType: String
-  });
-  
+  name: String,
+  data: Buffer,
+  contentType: String
+});
+
 // Create a model for the image
 const Image = mongoose.model('Image', imageSchema);
 
 // Create a Multer storage engine that stores files in memory
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Define a route to handle file uploads
 app.post('/upload', upload.single('file'), async (req, res) => {
-    try {
-      const image = new Image({
-        name: req.file.originalname,
-        data: req.file.buffer,
-        contentType: req.file.mimetype
-      });
-      await image.save();
-      res.send('File uploaded successfully');
-      console.log('done')
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server error');
-    }
-  });
+  try {
+    const image = new Image({
+      name: req.file.originalname,
+      data: req.file.buffer,
+      contentType: req.file.mimetype
+    });
+    await image.save();
+    res.send('File uploaded successfully');
+    console.log('done');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 
-////////////////////////////////////////////////////
+// Define a route to get all images
+app.get('/images', async (req, res) => {
+  try {
+    const images = await Image.find({});
+    res.json(images);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 
-//router
-app.use('/', routerTest)
+// router
+app.use('/', routerTest);
 
-//port
-const PORT=process.env.PORT||8080
+// port
+const PORT = process.env.PORT || 8080;
 
-//server
-app.listen(PORT, ()=>console.log(`Server is running and listening to port ${PORT}`))
+// server
+app.listen(PORT, () => console.log(`Server is running and listening to port ${PORT}`));
