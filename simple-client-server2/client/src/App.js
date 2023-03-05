7c86f06397cff6b427f08f7a462972e3eebddb08
clearsky1359 <ali_hsd@yahoo.com>
2023-03-05 00:24:59 +0000

    image upload added

--- simple-client-server2/client/src/App.js
+++ simple-client-server2/client/src/App.js
@@ -7,6 +7,39 @@ function App() {
   const [data, setData]=useState('');
   const [name, setName]=useState('');
   const [email, setEmail]=useState('');
+  const [isLoading, setIsLoading] = useState(false);
+/////////////////////////////////////////////////////
+const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  }
+
+  const handleSubmit2 = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append('file', file);
+    try {
+      const response = await fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/upload', {
+        method: 'POST',
+        body: formData
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  
+
+////////////////////////////////////////////////
+
+
+
 
   useEffect(()=>{
     fetch('https://8080-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io/getAllTests')
@@ -51,6 +84,14 @@ const handleSubmit=(e)=>{
         <button>Submit</button>
         <hr/><hr/>
       </form>
+      <form onSubmit={handleSubmit2}>
+        <input type="file" onChange={handleFileChange} />
+        {isLoading && <p>Loading...</p>}
+        <button type="submit">Submit</button>
+      </form>
+
+
+
 
       {data && data.map((r,index) => (
         <div key={index}>
--- simple-client-server2/server/app.js
+++ simple-client-server2/server/app.js
@@ -3,6 +3,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 const routerTest=require('./routes/test2')
+const multer  = require('multer');
 require('dotenv').config();
 
 //app
@@ -10,10 +11,50 @@ const app=express()
 
 //middleware
 app.use(express.json())
-app.use(cors({origin:'https://3000-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io'}))
-
+//app.use(cors({origin:'https://3000-clearsky135-reactprojec-pn9cq9isfm9.ws-us89.gitpod.io'}))
+// Configure CORS headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 mongoose.connect(process.env.URI).then(()=>console.log('Connected to DB'))
+
+//////////////////////////////////////////////////
+// Create a schema for the image
+const imageSchema = new mongoose.Schema({
+    name: String,
+    data: Buffer,
+    contentType: String
+  });
+  
+// Create a model for the image
+const Image = mongoose.model('Image', imageSchema);
+
+// Create a Multer storage engine that stores files in memory
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+// Define a route to handle file uploads
+app.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+      const image = new Image({
+        name: req.file.originalname,
+        data: req.file.buffer,
+        contentType: req.file.mimetype
+      });
+      await image.save();
+      res.send('File uploaded successfully');
+      console.log('done')
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  });
+
+////////////////////////////////////////////////////
 
 //router
 app.use('/', routerTest)
